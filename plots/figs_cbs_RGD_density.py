c51ee793f97e431275b92e8bb909d7b7597173ff
Nicolas Aguilera Garcia <niagar25@gmail.com>
2026-03-23 14:45:00 -0500

    [add] scale factor

---
+++ plots/figs_cbs_RGD_density.py
@@ -0,0 +1,282 @@
+from scipy.sparse.linalg._isolve.lsqr import eps
+from utils.loaders import load_sweep
+from utils.styles import apply
+import utils.figures as figures
+import utils.cbs as cbs
+from scipy.ndimage import uniform_filter1d
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+apply(context="paper", col="single")
+
+save_path = "/Users/niaggar/Documents/Thesis/Results"
+folder_base = "Data/cbs/"
+
+
+def get_sweep_name(radius, polarization):
+    name = f"cbs_RGD_sweep_poly_densities_rad-{radius:.3f}-{polarization}-1000M_photons"
+    return name
+
+
+volume_fraction_s = [0.05, 0.07, 0.10, 0.20, 0.30]
+polarization = ["linear", "circular"]
+radius_list = [0.085, 0.100]
+
+
+data = {}
+
+theta_degrees = np.linspace(0, 5, 600)
+theta_mrad = theta_degrees * (1000 * np.pi / 180)
+phi_degrees = np.linspace(0, 360, 80)
+
+MAX_MRAD = 70
+N_PHOTONS = 1_000_000_000
+
+t_max = 50.0
+d_time = 5.0
+n_time_bins = int(t_max / d_time)
+
+for r in radius_list:
+    linear = get_sweep_name(r, polarization[1])
+    circular = get_sweep_name(r, polarization[0])
+    linear_raw = load_sweep(f"{folder_base}/{linear}")
+    circular_raw = load_sweep(f"{folder_base}/{circular}")
+
+    data[f"{r}"] = {}
+    data_current = {}
+
+    for run_name in circular_raw.keys():
+        circular_loader = circular_raw[run_name]
+        linear_loader = linear_raw[run_name]
+
+        radius = circular_loader.params["radius_um"]
+        volume_fraction = circular_loader.params["volume_fraction"]
+        n_particle = circular_loader.params["n_particle"]
+        n_medium = circular_loader.params["n_medium"]
+        m_relative = circular_loader.params["m_relative"]
+
+        wavelength = circular_loader.params["wavelength_um"]
+        mean_free_path = circular_loader.params["mean_free_path_ls_um"]
+        transport_mean_free_path = circular_loader.params["transport_mean_free_path_lstar_um"]
+
+        scattering_efficiency = circular_loader.params["scattering_efficiency"]
+        mu_scattering = circular_loader.params["mu_scattering_um_inv"]
+        mu_absortion = circular_loader.params["mu_absortion_um_inv"]
+        anisotropy_factor = circular_loader.params["anisotropy_factor"]
+        size_parameter = circular_loader.params["size_parameter"]
+        condition_1 = circular_loader.params["condition_1"]
+        condition_2 = circular_loader.params["condition_2"]
+
+        theta_coherent = 1 / (2 * np.pi * n_medium * transport_mean_free_path / wavelength) * 180 / np.pi  # Convertir a grados
+        theta_coherent_milirad = theta_coherent * (1000 * np.pi / 180)
+
+        # print(f"Anisotropy factor for radius {radius:.3f}: {anisotropy_factor:.4f}")
+        # print(f"Condition 1 (|m-1|): {condition_1:.4f}")
+        # print(f"Condition 2 (size parameter * |m-1|): {condition_2:.4f}")
+        # print(f"Transport mean free path: {transport_mean_free_path:.4f}")
+        # print(f"Coherent backscattering angle (degrees): {theta_coherent:.4f}")
+        # print(f"Coherent backscattering angle (mrad): {theta_coherent_milirad:.4f}")
+
+        params = circular_loader.params
+        data_current = {
+            "anisotropy": anisotropy_factor,
+            "mean_free_path": mean_free_path,
+            "transport_mean_free_path": transport_mean_free_path,
+            "scattering_efficiency": scattering_efficiency,
+            "volume_fraction": volume_fraction,
+            "radius": radius,
+            "size_parameter": size_parameter,
+            "theta_coherent": theta_coherent,
+            "theta_coherent_mrad": theta_coherent_milirad,
+            "ff_circular_timed": [{
+                "coherent": {
+                    "s0": circular_loader.derived(f"farfield_cbs_timed_{t}/coherent/s0"),
+                    "s1": circular_loader.derived(f"farfield_cbs_timed_{t}/coherent/s1"),
+                    "s2": circular_loader.derived(f"farfield_cbs_timed_{t}/coherent/s2"),
+                    "s3": circular_loader.derived(f"farfield_cbs_timed_{t}/coherent/s3"),
+                },
+                "incoherent": {
+                    "s0": circular_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s0"),
+                    "s1": circular_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s1"),
+                    "s2": circular_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s2"),
+                    "s3": circular_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s3"),
+                },
+                "enhancement": {},
+            } for t in range(n_time_bins)],
+            "ff_linear_timed": [{
+                "coherent": {
+                    "s0": linear_loader.derived(f"farfield_cbs_timed_{t}/coherent/s0"),
+                    "s1": linear_loader.derived(f"farfield_cbs_timed_{t}/coherent/s1"),
+                    "s2": linear_loader.derived(f"farfield_cbs_timed_{t}/coherent/s2"),
+                    "s3": linear_loader.derived(f"farfield_cbs_timed_{t}/coherent/s3"),
+                },
+                "incoherent": {
+                    "s0": linear_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s0"),
+                    "s1": linear_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s1"),
+                    "s2": linear_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s2"),
+                    "s3": linear_loader.derived(f"farfield_cbs_timed_{t}/incoherent/s3"),
+                },
+                "enhancement": {},
+            } for t in range(n_time_bins)],
+        }
+
+        data[f"{r}"][run_name] = data_current
+
+        # Calculate the intensities and enhancement factors for each time bin and scattering order, and store them in the data structure. Use a small epsilon to avoid division by zero when calculating enhancement factors.
+        eps = 1e-30  # pure numerical guard, no physical effect
+
+        for t in range(n_time_bins):
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Ico"] = (data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["s0"] - data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Icross"] = (data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["s0"] + data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Itotal"] = data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Ico"] + data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Icross"]
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Ico"] = (data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["s0"] - data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Icross"] = (data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["s0"] + data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Itotal"] = data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Ico"] + data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Icross"]
+
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["enhancement"]["Ico"] = (
+                (data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Ico"] + eps)
+                / (data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Ico"] + eps)
+            )
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["enhancement"]["Icross"] = (
+                (data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Icross"] + eps)
+                / (data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Icross"] + eps)
+            )
+            data[f"{r}"][run_name]["ff_circular_timed"][t]["enhancement"]["total"] = (
+                (data[f"{r}"][run_name]["ff_circular_timed"][t]["coherent"]["Itotal"] + eps)
+                / (data[f"{r}"][run_name]["ff_circular_timed"][t]["incoherent"]["Itotal"] + eps)
+            )
+
+        for t in range(n_time_bins):
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Ico"] = (data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["s0"] - data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Icross"] = (data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["s0"] + data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Itotal"] = data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Ico"] + data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Icross"]
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Ico"] = (data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["s0"] - data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Icross"] = (data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["s0"] + data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["s3"]) / 2
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Itotal"] = data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Ico"] + data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Icross"]
+
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["enhancement"]["Ico"] = (
+                (data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Ico"] + eps)
+                / (data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Ico"] + eps)
+            )
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["enhancement"]["Icross"] = (
+                (data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Icross"] + eps)
+                / (data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Icross"] + eps)
+            )
+            data[f"{r}"][run_name]["ff_linear_timed"][t]["enhancement"]["total"] = (
+                (data[f"{r}"][run_name]["ff_linear_timed"][t]["coherent"]["Itotal"] + eps)
+                / (data[f"{r}"][run_name]["ff_linear_timed"][t]["incoherent"]["Itotal"] + eps)
+            )
+
+
+
+print("Data loaded and processed successfully.")
+
+
+
+# plot total intensityes for each type of particle. The total is on ff_timed[0], the rest of the time bins are for the temporal evolution
+fig, ax = plt.subplots(figsize=(10, 4))
+
+r = 0.100
+data_radius = data[f"{r}"]
+for run_name in data_radius.keys():
+    label = run_name
+    theta_coherent_milirad = data_radius[run_name]["theta_coherent_mrad"]
+
+    data = np.mean(data_radius[run_name]["ff_circular_timed"][0]["enhancement"]["Ico"], axis=1)
+    ax.plot(theta_mrad, data, label=label)
+
+    ax.axvline(theta_coherent_milirad, color="red", linestyle="--", alpha=0.7, label="Coherent angle" if run_name == list(data_radius.keys())[0] else None)
+
+ax.set_xlabel("Scattering Angle")
+ax.set_ylabel("Enhancement Factor")
+ax.legend()
+plt.tight_layout()
+
+
+
+
+
+
+
+
+# for run_name in run_names_circular:
+#     transport_mean_free_path = data_circular[run_name]["transport_mean_free_path"]
+#     theta_coherent_milirad = data_circular[run_name]["theta_coherent_mrad"]
+#     radius = data_circular[run_name]["radius"]
+#     volume_fraction = data_circular[run_name]["volume_fraction"]
+
+#     label = f"$a={radius:.3f}$, $f={volume_fraction:.3f}$"
+
+#     fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+
+#     ax_left.plot(theta_mrad, data_circular[run_name]["ff_timed"][0]["enhancement"]["Ico"], label=label)
+#     ax_left.axvline(theta_coherent_milirad, color="red", linestyle="--", alpha=0.7)
+#     ax_left.set_xlabel("Scattering angle (mrad)")
+#     ax_left.set_ylabel("Enhancement factor")
+
+#     ax_right.plot(theta_mrad, data_circular[run_name]["ff_timed"][0]["enhancement"]["Icross"], label=label)
+#     ax_right.axvline(theta_coherent_milirad, color="red", linestyle="--", alpha=0.7)
+#     ax_right.set_xlabel("Scattering angle (mrad)")
+#     ax_right.legend()
+
+#     plt.tight_layout()
+#     plt.savefig(f"{save_path}/cbs-RGD-ff-total-enhancement-ico-icross-{run_name}.pdf")
+
+
+# fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(10, 4))
+# for run_name in run_names_circular:
+#     transport_mean_free_path = data_circular[run_name]["transport_mean_free_path"]
+#     theta_coherent_milirad = data_circular[run_name]["theta_coherent_mrad"]
+#     radius = data_circular[run_name]["radius"]
+#     volume_fraction = data_circular[run_name]["volume_fraction"]
+
+#     label = f"$a={radius:.3f}$, $f={volume_fraction:.3f}$"
+
+#     ax_left.plot(theta_mrad, data_circular[run_name]["ff_timed"][0]["enhancement"]["Ico"], label=label)
+#     # ax_left.axvline(theta_coherent_milirad, color="red", linestyle="--", alpha=0.7)
+#     ax_left.set_xlabel("Scattering angle (mrad)")
+#     ax_left.set_ylabel("Intensity (a.u.)")
+#     # ax_left.set_ylabel("Enhancement factor")
+#     ax_left.set_title("Co-polarized")
+#     ax_left.set_xlim(0, 40)
+
+
+#     ax_right.plot(theta_mrad, data_circular[run_name]["ff_timed"][0]["enhancement"]["Icross"], label=label)
+#     # ax_right.axvline(theta_coherent_milirad, color="red", linestyle="--", alpha=0.7)
+#     ax_right.set_xlabel("Scattering angle (mrad)")
+#     ax_right.set_ylabel("Intensity (a.u.)")
+#     ax_right.set_title("Cross-polarized")
+#     ax_right.set_xlim(0, 40)
+#     ax_right.legend()
+
+# plt.tight_layout()
+# plt.savefig(f"{save_path}/cbs-RGD-ff-total-enhancement-ico-icross.pdf")
+
+
+
+# for run_name in run_names_circular:
+#     transport_mean_free_path = data_circular[run_name]["transport_mean_free_path"]
+#     theta_coherent_milirad = data_circular[run_name]["theta_coherent_mrad"]
+#     radius = data_circular[run_name]["radius"]
+#     volume_fraction = data_circular[run_name]["volume_fraction"]
+
+#     label = f"$a={radius:.3f}$, $f={volume_fraction:.3f}$"
+
+#     fig, (ax_left, ax_right) = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+
+#     for order in scattering_order_bins:
+#         ax_left.plot(theta_mrad, data_circular[run_name]["ff_order"][scattering_order_bins.index(order)]["coherent"]["Ico"], label=f"order {order}")
+#         ax_right.plot(theta_mrad, data_circular[run_name]["ff_order"][scattering_order_bins.index(order)]["coherent"]["Icross"], label=f"order {order}")
+
+#     plt.tight_layout()
+#     ax_right.legend()
+#     ax_left.set_xlabel("Scattering angle (mrad)")
+#     ax_left.set_ylabel("Enhancement factor")
+#     plt.savefig(f"{save_path}/cbs-RGD-ff-scattering-order-enhancement-ico-icross-{run_name}.pdf")
+
+
+
+
+plt.show()
---
+++ results/sim_cbs_RGD_density_scale_multiple.py
@@ -0,0 +1,211 @@
+import __main__
+import time
+import numpy as np
+
+from luminis_mc import (
+    SweepManager,
+    Laser, RGDMedium, Sample, FarFieldCBSSensor, StatisticsSensor, SensorsGroup, SimConfig, RayleighDebyeEMCPhaseFunction,
+    run_simulation_parallel, postprocess_farfield_cbs,
+    set_log_level, LogLevel, LaserSource
+)
+
+
+set_log_level(LogLevel.info)
+
+exp_name = "test-time-scale"
+base_dir = "/Users/niaggar/Documents/Thesis/Progress/16Mar26"
+
+
+sweep = SweepManager(exp_name, base_dir, timestamped=False)
+sweep.snapshot_master_script(__main__.__file__)
+sweep.log_readme("CBS Rayleigh-Gans-Debye Polyester particle, fixed radius, sweep over volume fraction -> mean free path")
+
+radius = 0.100
+f_scale = 50
+volume_fraction_s = [0.05, 0.06, 0.08, 0.10, 0.15]
+scaled_volume_fraction_s = [f * f_scale for f in volume_fraction_s]
+
+n_particle = 1.59
+n_medium = 1.33
+mu_absortion_percent = 0.0
+wavelength = 0.514
+
+# Laser parameters
+laser_m_polarization_state = 1/np.sqrt(2)
+laser_n_polarization_state = -1j/np.sqrt(2)
+laser_radius = 4.0
+laser_type = LaserSource.Gaussian
+
+# Phase function parameters
+phasef_theta_min = 0.0
+phasef_theta_max = np.pi
+phasef_ndiv = 100_000
+
+# Simulation parameters
+n_photons = 1_000_000
+
+# Sensor parameters
+theta_max_far_field = np.deg2rad(30)
+phi_max_far_field = 2 * np.pi
+n_theta_far_field = 600
+n_phi_far_field = 60
+d_theta = theta_max_far_field / n_theta_far_field
+d_phi = phi_max_far_field / n_phi_far_field
+
+# Light speed is always 1 nm/s
+t_max = 50.0
+d_time = 5.0
+
+# Statistics sensor parameters
+stats_t_max = t_max
+stats_d_time = d_time
+max_events = 200
+max_depth = 100
+n_depth = 10
+
+
+def run_single_simulation(exp, radius, volume_fraction):
+    phase = RayleighDebyeEMCPhaseFunction(wavelength, radius, n_particle, n_medium, phasef_ndiv, phasef_theta_min, phasef_theta_max)
+    medium = RGDMedium(phase, radius, n_particle, n_medium, wavelength)
+    sample = Sample(n_medium)
+    sample.add_layer(medium, 0.0, float('inf'))
+
+    scattering_efficiency = medium.scattering_efficiency()
+    mean_free_path = (4.0 * radius) / (3.0 * volume_fraction * scattering_efficiency)
+    inv_mean_free_path = 1 / mean_free_path
+
+    mean_free_path_real = f_scale * mean_free_path
+
+    mu_absortion = mu_absortion_percent * inv_mean_free_path
+    mu_scattering = inv_mean_free_path - mu_absortion
+
+    medium.set_mean_free_path(mean_free_path)
+    medium.set_scattering_coefficient(mu_scattering)
+    medium.set_absorption_coefficient(mu_absortion)
+
+    laser = Laser(laser_m_polarization_state, laser_n_polarization_state, wavelength, laser_radius * mean_free_path, laser_type)
+    anysotropy = phase.get_anisotropy_factor()
+    transport_mean_free_path = mean_free_path / (1 - anysotropy[0])
+    m_relative = n_particle / n_medium
+    size_parameter = 2 * np.pi * radius * n_medium / wavelength
+    condition_1 = np.abs(m_relative - 1)
+    condition_2 = size_parameter * np.abs(m_relative - 1)
+    k_medium = 2 * np.pi * n_medium / wavelength
+
+    max_theta_cbs = 1 / (k_medium * transport_mean_free_path)
+    max_theta_cbs_real = 1 / (k_medium * (mean_free_path_real / (1 - anysotropy[0])))
+
+    print("---- Simulation parameters -----")
+    print(f"scattering_efficiency {scattering_efficiency:.3f}:")
+    print(f"Calculated mean free path: {mean_free_path:.2f} micrometers, scattering coefficient: {mu_scattering:.4f} 1/micrometers")
+    print(f"Anisotropy factor for radius {radius:.3f}: {anysotropy[0]:.4f}")
+    print(f"Condition 1 (|m-1|): {condition_1:.4f}")
+    print(f"Condition 2 (size parameter * |m-1|): {condition_2:.4f}")
+    print(f"Maximum scattering angle for CBS: {np.rad2deg(max_theta_cbs):.2f} degrees")
+    sens = SensorsGroup()
+    det = sens.add_detector(FarFieldCBSSensor(theta_max_far_field, phi_max_far_field, t_max, d_theta, d_phi, d_time, False))
+    det.set_theta_limit(0, theta_max_far_field)
+
+    stats = sens.add_detector(StatisticsSensor(z=0, absorb=True))
+    stats.set_theta_limit(0, theta_max_far_field)
+    stats.set_time_resolution(stats_t_max, stats_d_time)
+    stats.set_events_histogram_bins(max_events)
+    stats.set_theta_histogram_bins(0, theta_max_far_field, 180)
+    stats.set_phi_histogram_bins(0, 2*np.pi, 360)
+    stats.set_depth_histogram_bins(max_depth, n_depth)
+
+    config = SimConfig()
+    config.n_photons = n_photons
+    config.sample = sample
+    config.detector = sens
+    config.laser = laser
+    config.track_reverse_paths = True
+    config.pin_threads_to_cores = False
+    config.n_threads = 6
+    config.show_progress = True
+
+
+    # 3) params
+    exp.log_params(
+        # --- 1. System & Physical Properties ---
+        radius_um=radius,
+        volume_fraction=volume_fraction,
+        n_particle=n_particle,
+        n_medium=n_medium,
+        m_relative=m_relative,
+
+        # --- 2. Calculated Optical Properties ---
+        scattering_efficiency=scattering_efficiency,
+        mu_scattering_um_inv=mu_scattering,
+        mu_absortion_um_inv=mu_absortion,
+        anisotropy_factor=anysotropy[0],
+        size_parameter=size_parameter,
+        condition_1=condition_1,
+        condition_2=condition_2,
+
+        # --- 3. The "Yardsticks" (CRITICAL FOR POST-PROCESSING) ---
+        mean_free_path_ls_um=mean_free_path,
+        mean_free_path_real_um=mean_free_path_real,
+        transport_mean_free_path_lstar_um=transport_mean_free_path,
+        transport_mean_free_path_real_lstar_um=mean_free_path_real / (1 - anysotropy[0]),
+
+        max_theta_cbs_rad=max_theta_cbs,
+        max_theta_cbs_real_rad=max_theta_cbs_real,
+
+        # --- 5. Laser & Simulation Config ---
+        wavelength_um=wavelength,
+        laser_m_polarization_state=str(laser_m_polarization_state), # Cast complex to string to avoid JSON errors
+        laser_n_polarization_state=str(laser_n_polarization_state),
+        n_photons=n_photons,
+
+        f_scale=f_scale,
+    )
+
+    # 4) run
+    t0 = time.time()
+    run_simulation_parallel(config)
+    print("runtime_s:", time.time() - t0)
+
+    # 5) guarda RAW del sensor
+    exp.save_sensor(det, "farfield_cbs")
+    exp.save_sensor(stats, "statistics")
+
+    # 6) guarda READY-TO-PLOT (recomendado)
+    cbs_total = postprocess_farfield_cbs(det, n_photons)
+
+    print("Saving derived data...")
+    print(det.hits)
+
+
+    theta  = np.linspace(0, det.theta_max, det.N_theta)
+    phi   = np.linspace(0, det.phi_max, det.N_phi)
+    exp.save_derived("axes/theta_mrad", theta)
+    exp.save_derived("axes/phi_rad", phi)
+
+    timed_N = len(cbs_total.coherent)
+    for t in range(timed_N):
+        s0_total_coh = np.array(cbs_total.coherent[t].S0, copy=False)
+        s1_total_coh = np.array(cbs_total.coherent[t].S1, copy=False)
+        s2_total_coh = np.array(cbs_total.coherent[t].S2, copy=False)
+        s3_total_coh = np.array(cbs_total.coherent[t].S3, copy=False)
+        s0_total_inc = np.array(cbs_total.incoherent[t].S0, copy=False)
+        s1_total_inc = np.array(cbs_total.incoherent[t].S1, copy=False)
+        s2_total_inc = np.array(cbs_total.incoherent[t].S2, copy=False)
+        s3_total_inc = np.array(cbs_total.incoherent[t].S3, copy=False)
+
+        exp.save_derived(f"farfield_cbs_timed_{t}/coherent/s0", s0_total_coh)
+        exp.save_derived(f"farfield_cbs_timed_{t}/coherent/s1", s1_total_coh)
+        exp.save_derived(f"farfield_cbs_timed_{t}/coherent/s2", s2_total_coh)
+        exp.save_derived(f"farfield_cbs_timed_{t}/coherent/s3", s3_total_coh)
+        exp.save_derived(f"farfield_cbs_timed_{t}/incoherent/s0", s0_total_inc)
+        exp.save_derived(f"farfield_cbs_timed_{t}/incoherent/s1", s1_total_inc)
+        exp.save_derived(f"farfield_cbs_timed_{t}/incoherent/s2", s2_total_inc)
+        exp.save_derived(f"farfield_cbs_timed_{t}/incoherent/s3", s3_total_inc)
+
+
+
+for i, volume in enumerate(scaled_volume_fraction_s):
+    print(f"Running simulation for volume fraction {volume:.2f} at {time.strftime('%Y-%m-%d %H:%M:%S')}")
+    run_name = f"volume_fraction_{volume:.2f}"
+    fun = lambda exp, r=radius, v=volume: run_single_simulation(exp, r, v)
+    sweep.run(i, run_name, fun)
